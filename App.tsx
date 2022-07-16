f738d0ac6c543f9c5416cd449ba784a26c26834e
Eduardo França <eduardo.franca.90@gmail.com>
2022-07-16 18:49:48 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default App;
